50696b0b5ecbb0d75115045c4c7e630050115cb6
Marco Urrea <marcourreab@gmail.com>
2021-01-11 19:59:00 -0300

    add docs

---
+++ docs/precache-manifest.16f43f87c5441a5986849e9a03657b7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32218b7a4730961732b5987915b7328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07cc81e2ed3579d5fd09",
+    "url": "/static/css/main.95a765f8.chunk.css"
+  },
+  {
+    "revision": "c891655c70870ca4f1e5",
+    "url": "/static/js/2.00eec8fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00eec8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07cc81e2ed3579d5fd09",
+    "url": "/static/js/main.25ce0861.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
